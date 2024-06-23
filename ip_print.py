7ce1e5ec2711545798bd29d8573bd17be47cbeba
Manasa R <manasar@Manasas-MacBook-Air.local>
2024-06-23 15:06:44 +0100

    print the ip addresses and network ips as well if present and handle error in json key

--- ip_print.py
+++ ip_print.py
@@ -17,3 +17,36 @@ def main(filename):
     except Exception as e:
         # Handles any other exception that arises
         print("Unexpected Error: ", e)
+        sys.exit(3)
+
+    try:
+        vm_private_ips = file_data["vm_private_ips"]["value"]
+    except KeyError:
+        # Handles the exception if the required Keys are not present in the JSON file
+        print(f"Error: 'vm_private_ips' or 'value' key not found in '{filename}'.", file=sys.stderr)
+        sys.exit(4)
+
+    # Process network information if present
+    network_ips = {}
+    if "network" in file_data:
+        for vm in file_data["network"]["vms"]:
+            name = vm["attributes"]["name"]
+            network_ips[name] = vm["attributes"]["access_ip_v4"]
+
+    # Print IP addresses
+    for name, ip in vm_private_ips.items():
+        # Get network IP
+        network_ip = network_ips.get(name, "")
+        # Print the IP address and network IP (if present) on the same line
+        print(ip, network_ip)
+
+
+if __name__ == "__main__":
+    # To verify the correct number of Command line arguments to run the script
+    if len(sys.argv) != 2:
+        print("Usage: ip_print FILENAME", file=sys.stderr)
+        sys.exit(5)
+    # Fetch the filename from the second argument in the command-line arguments
+    filename = sys.argv[1]
+    # Calls the main function with the provided filename
+    main(filename)
